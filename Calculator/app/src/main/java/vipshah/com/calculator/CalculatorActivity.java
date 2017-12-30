7063e8e8016186901164d8409b0a4d522c7a1951
vipul shah <vipulshah2010@gmail.com>
2017-12-30 14:50:03 +0530

    Simple Calculator app

---
+++ Calculator/app/src/main/java/vipshah/com/calculator/CalculatorActivity.java
@@ -0,0 +1,55 @@
+package vipshah.com.calculator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    private EditText number1Text;
+    private EditText number2Text;
+    private Button addButton;
+    private Button resetButton;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+        // Create the reference to layout widgets
+
+        number1Text = findViewById(R.id.number1Text);
+        number2Text = findViewById(R.id.number2Text);
+        addButton = findViewById(R.id.addButton);
+        resetButton = findViewById(R.id.resetButton);
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number1Text.setText("");
+                number2Text.setText("");
+                number1Text.requestFocus();
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String num1 = number1Text.getText().toString();
+                String num2 = number2Text.getText().toString();
+
+                int n1 = Integer.parseInt(num1);
+                int n2 = Integer.parseInt(num2);
+
+                int answer = n1 + n2;
+
+                Toast.makeText(CalculatorActivity.this,
+                        "Answer is " + answer, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
